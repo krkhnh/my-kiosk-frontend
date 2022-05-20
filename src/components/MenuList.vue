45ea9ae553decebbac4ab61cf9d3b10c462c3a41
krkhnh <krkhnh@gmail.com>
2022-05-20 14:45:58 +0900

    백엔드 요청 추가

--- src/components/MenuList.vue
+++ src/components/MenuList.vue
@@ -1,22 +1,27 @@
 <template>
   <div class="container border">
-    <div class="row">
-      <div class="col border">
-        Menu
-      </div>
-      <div class="col">
-        Menu
-      </div>
-      <div class="col">
-        Menu
+    <div class="row row-cols-3">
+      <div v-for="menu in menus" v-bind:key="menu.id" class="col border">
+        {{ menu.name + ' ' + menu.price + '원' }}
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'MenuList'
+  name: 'MenuList',
+  async created() {
+
+    const menus = (await axios.get('http://localhost:8080/menus')).data
+    console.log(menus)
+    this.menus = menus
+  },
+  data() {
+    return {menus: []}
+  }
 }
 </script>
 
