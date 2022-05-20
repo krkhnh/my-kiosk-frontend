6f39059d156712d51ebf093bc2409caebc875a41
krkhnh <krkhnh@gmail.com>
2022-05-20 11:12:06 +0900

    화면 초안

---
+++ src/components/MenuList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container border">
+    <div class="row">
+      <div class="col border">
+        Menu
+      </div>
+      <div class="col">
+        Menu
+      </div>
+      <div class="col">
+        Menu
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
+
+<style scoped>
+</style>
